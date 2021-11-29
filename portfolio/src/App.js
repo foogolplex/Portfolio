65c87f19f4e26686f49e167c6056a9440632a4e2
Robert Wood <robertwood@tuta.io>
2021-11-28 22:45:04 -0600

    Styled headers, navbar, form. Attempt to fix contact form functionality.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,148 +1,15 @@
 import './App.css';
-
-import React, { useState, useEffect, useRef } from "react";
-import p5 from "p5";
-import Grow from "@mui/material/Grow";
-import Box from "@mui/material/Box";
-import { Link } from 'react-scroll';
-import * as Scroll from 'react-scroll';
-//import * as THREE from 'three';
-import TRUNK from 'vanta/dist/vanta.trunk.min';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import React, { useState, useEffect, useRef } from "react";
+import {Navigation, Landing, MainContent} from './Components';
 
-let scroll = Scroll.animateScroll;
-
-function App() {
-  const [vantaEffect, setVantaEffect] = useState(0)
-  const myRef = useRef(null)
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(TRUNK({
-        el: myRef.current,
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00,
-        color: 0x076678,
-        backgroundColor: 0x282828,
-        chaos: 2.00,
-        p5: p5
-      }))
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destroy()
-    }
-  }, [vantaEffect])
-  const checked = true;
+var App = () => { 
+  const checked = true 
   return (
-    <div className="App">
-      <>
-      <Navbar expand="sm" bg="transparent" variant="dark" fixed="top">
-        <Container>
-          <Navbar.Brand>
-            {/* eslint-disable-next-line */}
-            <a onClick={scroll.scrollToTop()} href="#"><img
-              src="rabbit.png"
-              width="25"
-              height="25" 
-              /> 
-            </a>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto"> 
-              <Nav.Link>
-                <Link activeClass="active" to="about" spy={true} smooth={true} offset={-250} duration={20}>About</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-250} duration={20}>Projects</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link activeClass="active" to="certs" spy={true} smooth={true} offset={-250} duration={20}>Certificates</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link activeClass="active" to="hire" spy={true} smooth={true} offset={-250} duration={20}>Hire Me</Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      </>
-      <div className="App-header" ref={myRef}>
-      <Grow in={checked}
-        style={{ transformOrigin: '0 0 0' }}
-        {...(checked ? { timeout: 2000 } : {})}>
-        <Box>
-          <p>Hey there 👋,</p>
-          <h1>I'm Robert Lazarov.</h1>
-        </Box>
-      </Grow>
-      <Grow in={checked}
-        style={{ transformOrigin: '0 0 0' }}
-        {...(checked ? { timeout: 4000 } : {})}>
-        <Box> 
-          <h1>I'm a <span id="highlight">Backend Developer.</span></h1>
-        </Box> 
-      </Grow>
-      </div>
-      <div className="About-header">
-        <h1 name="about">About Me</h1>
-          <h5 style={{}}>
-            I'm capable of making a variety of backend applications in Node.js and Express.js primarily. I'm also comfortable
-            with a few front-end technologies such as React, Material UI, and Reactstrap. If I need it, I'll learn it.
-          </h5>
-          <h5>
-            Hobby-wise I mainly just do a bit of digital painting and write. I'm a simple guy.
-          </h5> 
-      </div>
-      <div className="About-header">
-        <h1 name="projects">Projects</h1>
-      </div>
-      <div className="About-header">
-        <h1 name="certs">Certificates</h1>
-        <div>
-          <div>
-
-          <Button variant="outline-secondary">
-            <a target="_blank" rel="noopener noreferrer" href="https://www.udemy.com/certificate/UC-4295c6b1-0f58-4e76-847e-30144d7d258c/">Discrete Math</a>
-          </Button>
-          </div>
-          <div>
-          
-          <Button variant="outline-secondary">
-            <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/certification/robdev/javascript-algorithms-and-data-structures">Javascript Data Structures & Algorithms</a>
-          </Button>
-          
-          </div>
-        </div>
-      </div>
-      <div className="Hire-me">
-        <h1 name="hire">Hire Me</h1>
-        <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message"></textarea>
-        </form>
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
+    <div className="App"> 
+      <Navigation />
+      <Landing checked={checked} /> 
+      <MainContent />
     </div>
   );
 }
---
+++ portfolio/src/Components.js
@@ -0,0 +1,162 @@
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import Grow from "@mui/material/Grow";
+import Box from "@mui/material/Box";
+import { Link } from 'react-scroll';
+import * as Scroll from 'react-scroll';
+
+let scroll = Scroll.animateScroll;
+
+function Project(props){
+    return(
+        <div className="col" style={{textAlign: 'center'}}>
+            <h4 className="display-7">{props.ProjName}</h4>
+            <div className="thumbnail">
+
+            </div>
+            <Button variant="outline-secondary" href={props.Demo}>Live Demo</Button>
+            <Button variant="outline-secondary" href={props.Github}>Source</Button>
+            <h6>
+                {props.Description}
+            </h6>
+            <div className="tech-stack">
+                
+            </div> 
+        </div> 
+    );
+}
+function Form(){
+  return (
+    <div>
+      <form name="contact" method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <div style={{textAlign: 'center'}}>
+            <p>
+            <label><div>Your Name:</div><input type="text" name="name"/></label>
+            </p>
+            <p>
+            <label><div>Your Email:</div><input type="email" name="email"/></label>
+            </p>
+            <p>
+            <label><div>Message:</div><textarea name="message"></textarea></label>
+            </p>
+            <p>
+            <Button size="lg" variant="outline-secondary" type="submit" style={{color: "white"}}>Send</Button>
+            </p>
+        </div>
+      </form>
+    </div>
+  );
+}
+var Landing = (props) => {
+  var checked = props.checked;
+  var myRef = props.myRef;
+  return (
+    <div className="App-header" ref={myRef}>
+      <Grow in={checked}
+        style={{ transformOrigin: '0 0 0' }}
+        {...(checked ? { timeout: 2000 } : {})}>
+        <Box>
+          <p>Hey there 👋,</p>
+          <h1 className="display-3">I'm Robert Lazarov.</h1>
+        </Box>
+      </Grow>
+      <Grow in={checked}
+        style={{ transformOrigin: '0 0 0' }}
+        {...(checked ? { timeout: 4000 } : {})}>
+        <Box> 
+          <h1 className="display-3">I'm a <span id="highlight">Backend Developer.</span></h1>
+        </Box> 
+      </Grow>
+    </div>
+  );
+}
+var MainContent = () => {
+  return (
+    <div>
+      <div className="About-header">
+        <h1 name="About" className="display-3">About</h1>
+        <h5>
+          I'm capable of making a variety of backend applications primarily in <span id="highlight"> Node.js </span> and <span id="highlight"> Express.js</span>. I'm also comfortable
+          with a few front-end technologies such as <span id="highlight">React</span>
+            , <span id="highlight">Material UI</span>, and <span id="highlight">Reactstrap</span>.<br></br> If I <strong>need</strong> it, I'll <strong>learn</strong> it.
+        </h5>
+      </div>
+      <div className="Projects-header">
+        <h1 name="Projects" className="display-3" style={{paddingBottom: '5%'}}>Projects (Under Construction)</h1>
+        <div className="row" style={{alignItems: 'center'}}>
+            <Project ProjName="Timestamp Microservice" Description="This project is a timestamp microservice that serves timestamps based on parameters given in the url.
+                    The stack is Jest for unit testing, React for the front end, and Express.js as the framework for the backend." Demo="https://timest-microservice.heroku.app" Github="https://github.com/foogolplex/timestamp-microservice"/> 
+            <Project ProjName="Timestamp Microservice" Description="This project is a timestamp microservice that serves timestamps based on parameters given in the url.
+                    The stack is Jest for unit testing, React for the front end, and Express.js as the framework for the backend." Demo="https://timest-microservice.heroku.app" Github="https://github.com/foogolplex/timestamp-microservice"/> 
+            <div className="w-100" style={{paddingBottom: '5%'}}></div> 
+            <Project ProjName="Timestamp Microservice" Description="This project is a timestamp microservice that serves timestamps based on parameters given in the url.
+                    The stack is Jest for unit testing, React for the front end, and Express.js as the framework for the backend." Demo="https://timest-microservice.heroku.app" Github="https://github.com/foogolplex/timestamp-microservice"/> 
+            <Project ProjName="Timestamp Microservice" Description="This project is a timestamp microservice that serves timestamps based on parameters given in the url.
+                    The stack is Jest for unit testing, React for the front end, and Express.js as the framework for the backend." Demo="https://timest-microservice.heroku.app" Github="https://github.com/foogolplex/timestamp-microservice"/> 
+        </div>
+      </div>
+      <div className="Certs-header">
+        <h1 name="Certs" className="display-3">Certification</h1>
+        <Certs />
+      </div>
+      <div className="Hire-me">
+        <h1 name="Hire Me" className="display-3">Hire Me.</h1>
+        <Form />
+      </div>
+    </div>
+  );
+}
+function Certs(){
+  return (
+    <div>
+      <div style={{textAlign: 'center', paddingTop: '5%', paddingBottom: '5%'}}>
+        <Button variant="outline-secondary" size="lg">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.udemy.com/certificate/UC-4295c6b1-0f58-4e76-847e-30144d7d258c/">Discrete Math</a>
+        </Button>
+      </div>
+      <div>
+        <Button variant="outline-secondary" size="lg">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/certification/robdev/javascript-algorithms-and-data-structures">Javascript Data Structures & Algorithms</a>
+        </Button>
+      </div>
+    </div>
+  );
+}
+function NavLink(props){
+  return (
+      <Link activeClass="active" to={props.Text} spy={true} smooth={true} offset={-250} duration={20}>
+    <Nav.Link>
+      {props.Text}
+    </Nav.Link></Link>
+  );
+}
+var Navigation = () => {
+  return (
+    <>
+      <Navbar expand="sm" bg="transparent" variant="dark" fixed="top">
+        <Container>
+          <Navbar.Brand>
+            {/* eslint-disable-next-line */}
+            <a onClick={scroll.scrollToTop()} href="#"><img
+              src="rabbit.png"
+              width="25"
+              height="25" 
+              /> 
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto nav nav-tabs"> 
+              <NavLink Text="About" />
+              <NavLink Text="Projects" />
+              <NavLink Text="Certs" />
+              <NavLink Text="Hire Me" />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export {Navigation, MainContent, Landing};
